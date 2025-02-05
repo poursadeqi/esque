1cc2797dd5bdab01e4144e929251100a87b72798
ebrahim <ebrahim.poursadeghi@real-digital.de>
2025-02-05 21:45:03 +0100

    add support for proto

--- esque/io/messages.py
+++ esque/io/messages.py
@@ -1,3 +1,4 @@
+import base64
 import dataclasses
 import datetime
 from typing import List, NamedTuple, Optional, Union
@@ -8,20 +9,22 @@ class MessageHeader(NamedTuple):
     value: Optional[str]
 
 
+PrimaryTypes = Union[dict, list, tuple, str, int, float, bool, bytes, None]
+
 @dataclasses.dataclass
 class MessagePayload:
-    PrimaryTypes = Union[dict, list, tuple, str, int, float, bool, bytes, None]
     payload: PrimaryTypes = None
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.payload is None
 
-    def is_printable(self):
-        return type(self.payload) is not bytes
+    def is_printable(self) -> bool:
+        return not isinstance(self.payload, bytes)
 
-    def __repr__(self):
-        return self.payload
-
+    def __repr__(self) -> str:
+        if self.is_printable():
+            return repr(self.payload)
+        return base64.b64encode(self.payload).decode("utf-8")
 
 def now_utc() -> datetime.datetime:
     return datetime.datetime.now(tz=datetime.timezone.utc)
